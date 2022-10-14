5e47051d9a6f3be9bcb019137b0faa8dc85cd75f
canasmh <canasmh@yahoo.com>
2022-10-14 12:30:25 -0400

    Create the form fields

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -3,7 +3,51 @@ import React from "react";
 
 function Form(props) {
 
-    return (<></>);
+    return (
+    <>
+        <h1>Join the Community</h1>
+        <p>Fill out the form below: </p>
+
+        <form>
+            <label htmlFor="fName">First Name: </label>
+            <input 
+                type="text"
+                id="fName"
+                name="fName"
+            />
+            <br />
+            <label htmlFor="lName">Last Name: </label>
+            <input 
+                type="text"
+                id="lName"
+                name="lName"
+            />
+            <br />
+            <label htmlFor="email">Email Address: </label>
+            <input 
+                type="email"
+                id="email"
+                name="email"
+            />
+            <br />
+            <label htmlFor="password">Password: </label>
+            <input 
+                type="password"
+                id="password"
+                name="password"
+            />
+            <br />
+            <label htmlFor="agree">Do you agree to the terms and servies? </label>
+            <input 
+                type="checkbox"
+                id="agree"
+                name="agree"
+            />
+            <br />
+            <button type="submit">Submit</button>
+        </form>
+    </>
+    );
 }
 
 export default Form;
